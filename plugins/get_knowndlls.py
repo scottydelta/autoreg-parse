f6a2ed314e9349b0247f7edbf6c291fe4980444e
Patrick Olsen <patrickolsen@sysforensics.org>
2013-12-31 20:44:23 +0800

    Re-wrote Known DLLs Plugin
    Clean up the code.

--- plugins/get_knowndlls.py
+++ plugins/get_knowndlls.py
@@ -1,24 +1,17 @@
 from Registry import Registry
+from get_controlset import getControlSet
 
 def getPlugin(reg_sys, reg_nt='', reg_soft=''):
 
-    selectCurrent = reg_sys.open("Select")
-    selectCurrentNumber = selectCurrent.value("Current").value()
-    controlSetSubkeys = reg_sys.open('ControlSet00%d\\Control' % (selectCurrentNumber))
-
-    known_DLLs_list = [('ControlSet00%d\\' % (selectCurrentNumber)) + controlSetSubkeys.name() + "\\Session Manager\\KnownDLLs"]
+    current = getControlSet(reg_sys)
+    knowndlls = reg_sys.open('%s\\Control\\Session Manager\\KnownDLLs' % (current))
 
     print ("\n" + ("=" * 51) + "\nKNOWN DLLs\n" + ("=" * 51))
+    print '\nKnown DLLs LastWrite: %s\n' % (knowndlls.timestamp())
 
     try:
-        for k in known_DLLs_list:
-            key = reg_sys.open(k)
-            for v in key.values():
-                if v.value_type() == Registry.RegSZ or v.value_type() == Registry.RegExpandSZ or v.value_type() == Registry.RegMultiSZ:
-                    print 'Key: %s\nValue: %s\n' % (str(v.name()).encode('ascii', 'ignore'), str(v.value()).encode('ascii', 'ignore'))
-
-                else:
-                    pass
+        for v in knowndlls.values():
+            print 'Name: %s\nDLL: %s\n' % (v.name(), v.value())
 
     except Registry.RegistryKeyNotFoundException as e:
         pass
