a74b79c1214f9b4c2939b375a56113e008fdf3e3
Patrick Olsen <patrickolsen@sysforensics.org>
2013-12-31 17:20:15 +0800

    Added LastWrite Times and Display Names to services

--- plugins/get_services.py
+++ plugins/get_services.py
@@ -9,8 +9,11 @@ def getPlugin(reg_sys, reg_nt='', reg_soft=''):
 
     service_list = []
     autostart_list = []
+    loadondemand_list = []
+    bootloader_list = []
     autostart_dict = defaultdict(list)
     loadondemand_dict = defaultdict(list)
+    bootloader_dict = defaultdict(list)
     
     service_baseline = []
     baseline = open("plugins/service_baseline.txt", 'r').read()
@@ -18,10 +21,8 @@ def getPlugin(reg_sys, reg_nt='', reg_soft=''):
 
     for service in servicesnames .subkeys():
         service_list.append(service.name().lower())
-
     for service_name in service_list:
         k = reg_sys.open('%s\\Services\\%s' % (current, service_name))
-    
         for v in k.values():
             if v.name() == "Start":
                 start_methods = v.value()
@@ -30,43 +31,72 @@ def getPlugin(reg_sys, reg_nt='', reg_soft=''):
                     if service_start_code == "2": 
                         autostart_list.append(k.name())
                         try:
+                            display_name = k.value("DisplayName").value()
                             image_path = k.value("ImagePath").value()
                         except:
+                            display_name = "???"
                             image_path = "No Image Path Found!"
                         autostart_dict['ServiceName'].append(k.name().lower())
                         autostart_dict['WriteTime'].append(k.timestamp())
                         autostart_dict['ImagePath'].append(image_path.lower())
+                        autostart_dict['DisplayName'].append(display_name)
                     # 0x3 (Load on demand) = SCM - Not start until the user starts it.
                     elif service_start_code == "3": 
-                        autostart_list.append(k.name())
                         try:
+                            display_name = k.value("DisplayName").value()
                             image_path = k.value("ImagePath").value()
                         except:
+                            display_name = "???"
                             image_path = "No Image Path Found!"
-                        
                         loadondemand_dict['ServiceName'].append(k.name().lower())
                         loadondemand_dict['WriteTime'].append(k.timestamp())
                         loadondemand_dict['ImagePath'].append(image_path.lower())
-                        pass
-                    
+                        loadondemand_dict['DisplayName'].append(display_name)
+                    # 0x0 (Boot) = Kernel Loader
+                    elif service_start_code == "0": 
+                        bootloader_list.append(k.name())
+                        try:
+                            display_name = k.value("DisplayName").value()
+                            image_path = k.value("ImagePath").value()
+                        except:
+                            display_name = "???"
+                            image_path = "No Image Path Found!"
+                        bootloader_dict['ServiceName'].append(k.name().lower())
+                        bootloader_dict['WriteTime'].append(k.timestamp())
+                        bootloader_dict['ImagePath'].append(image_path.lower())
+                        bootloader_dict['DisplayName'].append(display_name)             
             else:
                 pass
 
     print ("\n" + ("=" * 51) + "\nUNKNOWN/NON-BASELINED TYPE 2 SERVICES)\n" + ("=" * 51))
-    for sname, ltime, ipath in izip(autostart_dict['ServiceName'], autostart_dict['WriteTime'], autostart_dict['ImagePath']):
-    #for sname, ltime, ipath in izip(autostart_dict['ServiceName'], autostart_dict['WriteTime'].append(k.timestamp()), autostart_dict['ImagePath']):
-        #print "Meh1"
-        #print sname, ltime, ipath
+    for sname, ltime, ipath, dispname in izip(autostart_dict['ServiceName'], autostart_dict['WriteTime'], autostart_dict['ImagePath'], autostart_dict['DisplayName']):
         for name in service_baseline:
             if sname.lower() in name.lower():
                 pass
             else:
-                print 'Service Name: %s\nLastWrite: %s\nImage Path: %s\n' % (sname, ltime, ipath.encode('ascii', 'ignore'))
+                print "Service Name: %s\nLastWrite: %s\nImage Path: %s\nDisplay Name: %s\n" % (sname, ltime, ipath.encode('ascii', 'ignore'), dispname)
       
     print ("\n" + ("=" * 51) + "\nTYPE 2 SERVICES NOT IN SYSTEM32\n" + ("=" * 51))
     
-    for sname, ltime, ipath  in izip(autostart_dict['ServiceName'], loadondemand_dict['WriteTime'], autostart_dict['ImagePath']):
+    for sname, ltime, ipath, dispname in izip(autostart_dict['ServiceName'], autostart_dict['WriteTime'], autostart_dict['ImagePath'], autostart_dict['DisplayName']):
         if "system32" not in ipath.lower():
-            print "Service Name: %s\nLastWrite: %s\nImage Path: %s\n" % (sname, ltime, ipath.encode('ascii', 'ignore'))
+            print "Service Name: %s\nLastWrite: %s\nImage Path: %s\nDisplay Name: %s\n" % (sname, ltime, ipath.encode('ascii', 'ignore'), dispname)
         else:
             pass
+
+    print ("\n" + ("=" * 51) + "\nALL TYPE 2 SERVICES\n" + ("=" * 51))
+    
+    for sname, ltime, ipath, dispname in izip(autostart_dict['ServiceName'], autostart_dict['WriteTime'], autostart_dict['ImagePath'], autostart_dict['DisplayName']):   
+        print "Service Name: %s\nLastWrite: %s\nImage Path: %s\nDisplay Name: %s\n" % (sname, ltime, ipath.encode('ascii', 'ignore'), dispname)
+
+    
+    print ("\n" + ("=" * 51) + "\nALL TYPE 3 SERVICES\n" + ("=" * 51))
+    for sname, ltime, ipath, dispname in izip(loadondemand_dict['ServiceName'], loadondemand_dict['WriteTime'], loadondemand_dict['ImagePath'], loadondemand_dict['DisplayName']):   
+        print "Service Name: %s\nLastWrite: %s\nImage Path: %s\nDisplay Name: %s\n" % (sname, ltime, ipath.encode('ascii', 'ignore'), dispname)
+
+    
+    print ("\n" + ("=" * 51) + "\nALL TYPE 0 SERVICES\n" + ("=" * 51))
+    for sname, ltime, ipath, dispname in izip(bootloader_dict['ServiceName'], bootloader_dict['WriteTime'], bootloader_dict['ImagePath'], bootloader_dict['DisplayName']):   
+        print "Service Name: %s\nLastWrite: %s\nImage Path: %s\nDisplay Name: %s\n" % (sname, ltime, ipath.encode('ascii', 'ignore'), dispname)
+
+
