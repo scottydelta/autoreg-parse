1324c197477d39a221b9028a18070fa1123d208b
Patrick Olsen <patrickolsen@sysforensics.org>
2013-12-31 16:05:03 +0800

    Added lastwrite time to service keys

--- plugins/get_services.py
+++ plugins/get_services.py
@@ -34,6 +34,7 @@ def getPlugin(reg_sys, reg_nt='', reg_soft=''):
                         except:
                             image_path = "No Image Path Found!"
                         autostart_dict['ServiceName'].append(k.name().lower())
+                        autostart_dict['WriteTime'].append(k.timestamp())
                         autostart_dict['ImagePath'].append(image_path.lower())
                     # 0x3 (Load on demand) = SCM - Not start until the user starts it.
                     elif service_start_code == "3": 
@@ -44,6 +45,7 @@ def getPlugin(reg_sys, reg_nt='', reg_soft=''):
                             image_path = "No Image Path Found!"
                         
                         loadondemand_dict['ServiceName'].append(k.name().lower())
+                        loadondemand_dict['WriteTime'].append(k.timestamp())
                         loadondemand_dict['ImagePath'].append(image_path.lower())
                         pass
                     
@@ -51,18 +53,20 @@ def getPlugin(reg_sys, reg_nt='', reg_soft=''):
                 pass
 
     print ("\n" + ("=" * 51) + "\nUNKNOWN/NON-BASELINED TYPE 2 SERVICES)\n" + ("=" * 51))
-    
-    for sname, ipath  in izip(autostart_dict['ServiceName'], autostart_dict['ImagePath']):
+    for sname, ltime, ipath in izip(autostart_dict['ServiceName'], autostart_dict['WriteTime'], autostart_dict['ImagePath']):
+    #for sname, ltime, ipath in izip(autostart_dict['ServiceName'], autostart_dict['WriteTime'].append(k.timestamp()), autostart_dict['ImagePath']):
+        #print "Meh1"
+        #print sname, ltime, ipath
         for name in service_baseline:
             if sname.lower() in name.lower():
                 pass
             else:
-                print 'Service Name: %s\nImage Path: %s\n' % (sname, ipath.encode('ascii', 'ignore'))
+                print 'Service Name: %s\nLastWrite: %s\nImage Path: %s\n' % (sname, ltime, ipath.encode('ascii', 'ignore'))
       
     print ("\n" + ("=" * 51) + "\nTYPE 2 SERVICES NOT IN SYSTEM32\n" + ("=" * 51))
     
-    for sname, ipath  in izip(autostart_dict['ServiceName'], autostart_dict['ImagePath']):
+    for sname, ltime, ipath  in izip(autostart_dict['ServiceName'], loadondemand_dict['WriteTime'], autostart_dict['ImagePath']):
         if "system32" not in ipath.lower():
-            print "Service Name: %s\nImage Path: %s\n" % (sname, ipath.encode('ascii', 'ignore'))
+            print "Service Name: %s\nLastWrite: %s\nImage Path: %s\n" % (sname, ltime, ipath.encode('ascii', 'ignore'))
         else:
             pass
