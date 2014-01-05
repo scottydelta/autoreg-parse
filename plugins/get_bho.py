42f7f8bb294a20fc350b4a11c49f1c7984f4e33c
Patrick Olsen <patrickolsen@sysforensics.org>
2014-01-05 14:24:39 +0800

    Updated BHO Plugin

--- plugins/get_bho.py
+++ plugins/get_bho.py
@@ -3,21 +3,25 @@ from Registry import Registry
 def getPlugin(reg_soft, reg_nt='', reg_sys=''):
 
     bho_keys = ["Microsoft\\Windows\\CurrentVersion\\Explorer\\Browser Helper Objects",
-                "Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Browser Helper Objects"]
+                "WOW6432Node\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Browser Helper Objects"]
 
     BHO_list = []
 
     print ("\n" + ("=" * 51) + "\nBROWSER HELPER OBJECTS\n" + ("=" * 51))
 
-    try:
-        for b in bho_keys:
+    for b in bho_keys:
+        try:
             k = reg_soft.open(b)
             for v in k.subkeys():
                 BHO_list.append(v.name())
-
-        for clsids in BHO_list:
+        
+        except Registry.RegistryKeyNotFoundException as e:
+            pass
+    
+    for clsids in BHO_list:
+        try:
             ke = reg_soft.open("Classes\\CLSID\\%s" % (clsids))
-            print "Key Name: %s\nPath %s\nKey Last Write: %s\n" % (ke.name(), ke.subkey("InProcServer32").value('').value(), ke.timestamp())
+            print "KeyName: %s\nPath: %s\nLastWrite: %s\n" % (ke.name(), ke.subkey("InProcServer32").value('').value(), ke.timestamp())
+        except Registry.RegistryKeyNotFoundException as e:
+            pass
 
-    except Registry.RegistryKeyNotFoundException as e:
-        pass
